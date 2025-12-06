ef74b4fd903fa86e2dc700558f6bc1a7dc230564
Zilfs <rheinlandcast@gmail.com>
2025-12-07 00:43:02 +0700

    feat: added connection to database

--- src/Koneksi.java
+++ src/Koneksi.java
@@ -1,2 +1,19 @@
+import java.sql.*;
+import javax.swing.JOptionPane;
+
 public class Koneksi {
+    Connection koneksi;
+
+    public static Connection Koneksi(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection koneksi = DriverManager.getConnection("jdbc:mysql://localhost/toko_buku_uas_pbo","root","");
+
+            return koneksi;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+
+            return null;
+        }
+    }
 }
